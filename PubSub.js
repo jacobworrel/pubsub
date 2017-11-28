7adaf3683ffef17a73f986192265aca2375fc414
jacobworrel <jacobworrel@gmail.com>
2017-11-28 15:44:54 -0800

    make num of msg to subscribe to dynamic

--- PubSub.js
+++ PubSub.js
@@ -4,39 +4,45 @@ class PubSub {
     this.history = {};
   }
 
-  subscribe(topic, listener) {
+  subscribe(topic, listener, numMessages = 1) {
     if (!this.subscribers[topic]) {
       this.subscribers[topic] = [];
       this.history[topic] = [];
     }
-    this.subscribers[topic].push(listener);
+    this.subscribers[topic].push({ numMessages, listener });
   }
 
   publish(topic, message) {
-    if (!this.subscribers[topic]) {
+    if (!this.subscribers[topic] || !this.history[topic]) {
       throw new Error("Sorry! That topic hasn't been registered yet...");
     } else {
       const subscribers = this.subscribers[topic];
       const history = this.history[topic];
       history.push(message);
-      const lastTwoMessages = history.slice(-2);
-      subscribers.forEach(listener => listener(lastTwoMessages));
+      // const lastTwoMessages = history.slice(-2);
+      subscribers.forEach(subscriber => {
+        const { listener, numMessages } = subscriber;
+        const messages = history.slice(-numMessages);
+        listener(messages);
+      });
     }
   }
 }
 
+module.exports = PubSub;
 const listener1 = msg => console.log('listener1: ' + msg);
 const listener2 = msg => console.log('listener2: ' + msg);
 
 const pubsub = new PubSub();
 
-pubsub.subscribe('foo', listener1);
-pubsub.subscribe('foo', listener2);
-pubsub.subscribe('bar', listener2);
+// pubsub.subscribe('foo', listener1, 1);
+pubsub.subscribe('foo', listener2, 2);
+// pubsub.subscribe('bar', listener2);
 
 pubsub.publish('foo', 'hello');
-// pubsub.publish('foo', 'sup');
-// pubsub.publish('foo', 'morning');
-pubsub.publish('bar', 'world');
+// console.log('******************');
+pubsub.publish('foo', 'sup');
+// // pubsub.publish('foo', 'morning');
+// pubsub.publish('bar', 'world');
 
 // console.log(pubsub)
---
+++ test/test.js
@@ -0,0 +1,67 @@
+const expect = require('chai').expect;
+const PubSub = require('./../PubSub.js');
+
+describe('PubSub', () => {
+
+  describe('subscriptions', () => {
+    const { subscribers } = new PubSub();
+
+    it('should be initialized as an empty object', () => {
+      expect(subscribers).to.be.an('object');
+      expect(subscribers).to.be.empty;
+    });
+  });
+
+  describe('history', () => {
+    const { history } = new PubSub();
+
+    it('should be initialized as an empty object', () => {
+      expect(history).to.be.an('object');
+      expect(history).to.be.empty;
+    });
+  });
+
+  describe('subscribe', () => {
+    const pubsub = new PubSub();
+    const { subscribers } = pubsub;
+    const listener1 = msg => console.log('listener1: ' + msg);
+    const listener2 = msg => console.log('listener2: ' + msg);
+    const topic = 'foo';
+    pubsub.subscribe(topic, listener1);
+
+    it('should add a new topic to subscribe to', () => {
+      expect(subscribers).to.have.property(topic);
+      expect(subscribers[topic]).to.be.an('array');
+      expect(subscribers[topic]).to.have.lengthOf(1);
+    });
+
+    it('should add a listener that subscribes to a given topic', () => {
+      subscribers[topic].forEach(subscriber => {
+        expect(subscriber).to.be.an('object');
+        expect(subscriber.listener).to.be.a('function');
+      });
+    });
+
+    it('should have listeners default to subscribing to one message', () => {
+      subscribers[topic].forEach(subscriber => {
+        expect(subscriber.numMessages).to.be.a('number');
+        expect(subscriber.numMessages).to.equal(1);
+      })
+    });
+
+    it('should have listeners default to subscribing to one message', () => {
+      subscribers[topic].forEach(subscriber => {
+        expect(subscriber.numMessages).to.be.a('number');
+        expect(subscriber.numMessages).to.equal(1);
+      })
+    });
+
+    // it('should add a new topic ', () => {
+    //
+    // });
+  });
+
+  describe('publish', () => {
+
+  });
+});
